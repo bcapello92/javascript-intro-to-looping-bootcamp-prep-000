187f207e31ebe47786614b63ff5a06f18f7bb7e5
Brendan Capello <capello4@gmail.com>
2018-01-14 03:06:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 fucntion forLoop(array){
   for (var i = 0; i< 25; i++){
-    array[i]= "I am ${i} strange loop$"
+    array[i]= "I am ${i} strange loop${i === 0 ? '' : 's'}."
   }
   return array
 }
